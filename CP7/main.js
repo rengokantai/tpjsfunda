352fc4675e35101e272be4f837b319372aa3b6f1
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-18 13:09:18 -0500

    js pat 2-1

--- CP7/main.js
+++ CP7/main.js
@@ -14,3 +14,34 @@
     console.log(utils.trim(name));
 })();
 
+(function(){
+    var pubsub =(function(){
+        var subscribers = {};
+        function subscribe(e,cb){
+            if(!subscribers[e]){
+                var subscribeArr = [cb];
+                subscribers[e]=subscribeArr;
+            }else{
+                subscribeArr.push(cb);
+            }
+
+        }
+
+        function publish(e){
+            if(subscribers[e]){
+                subscribers[e].forEach(function(cb){
+                    cb();
+                });
+            }
+        }
+        return {
+            pub:publish,
+            sub:subscribe
+        };
+    })();
+    pubsub.sub('some',function(){
+        console.log('some');
+    })
+    pubsub.pub('some');
+})();
+
